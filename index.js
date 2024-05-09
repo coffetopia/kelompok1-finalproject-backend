35a9a9f295c957cf4efa9b5f3fa5b5977d0d41b4
Alvin Yusuf Riziq <alviniysfriz1@std.unissula.ac.id>
2024-05-09 19:51:39 +0700

    FEAT: menambahkan fitur routing

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const routes = require('./routes');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(cors());
+app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`App listen on http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App listen on http://localhost:${process.env.PORT}`);
 });
