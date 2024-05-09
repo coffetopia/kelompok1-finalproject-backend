4e2bd1127544cd7db10c38df9d9f1b8f2e5ea4a4
Alvin Yusuf Riziq <alviniysfriz1@std.unissula.ac.id>
2024-05-09 22:19:28 +0700

    FEAT: menambahkan fitur registrasi (unfix)

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const routes = require('./routes');
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use('/', routes);
 
 app.listen(process.env.PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const { where } = require('sequelize');
 const router = express.Router();
+const userModel = require('../models').user;
 
 router.get('/', (req, res) => {
   res.send('Home');
 });
+
+router.post('/register', (req, res) => {
+  userModel.create(req.body).then((success) => {
+    res.json({
+      status: true,
+      message: "User created successfully",
+    });
+  }).catch((error) => {
+    res.json({
+      status: false,
+      message: "Failed to register new user",
+    });
+  });
+});
+
+router.get('/register', (req, res) => {
+  userModel.findOne({
+    where: {
+      id: 1,
+    },
+  }).then((success) => {
+    console.log(success);
+    res.send(success);
+  }).catch((error) => {
+    console.log(error);
+    res.send(500);
+  });
+})
 
 module.exports = router;
