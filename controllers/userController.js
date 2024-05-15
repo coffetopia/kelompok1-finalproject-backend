1c6cc827fd80253345f1520c51246b4149b06574
Alvin Yusuf Riziq <alviniysfriz1@std.unissula.ac.id>
2024-05-15 23:29:22 +0700

    FINISH user only showUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,6 @@
 const userModel = require('../models').user;
 const response = require('../services/response');
 const getUsername = require('../services/getUsername');
-const { encryption } = require('../services/security');
 
 async function showProfile(req, res) {
   try {
@@ -27,50 +26,4 @@ async function showProfile(req, res) {
   }
 }
 
-async function editUser(req, res) {
-  try {
-    const usernameLogged = getUsername(req);
-    const { username, email, password } = req.body;
-    
-    const usernameRegistered = await userModel.findOne({
-      where: {
-        username,
-      },
-    });
-
-    const emailRegistered = await userModel.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (usernameRegistered !== null) {
-      return response(403, false, '', 'This username was registered', res);
-    }
-    
-    if (emailRegistered !== null) {
-      return response(403, false, '', 'This email was registered', res);
-    }
-
-    const hashedPassword = await encryption(password);
-
-    const update = await userModel.update(
-      {
-        username,
-        password: hashedPassword,
-        email,
-      },
-      {
-        where: {
-          username: usernameLogged,
-        },
-      },
-    );
-
-    response(201, true, update, 'User was edited successfully');
-  } catch (error) {
-    response(500, false, error, 'Server Error', res);
-  }
-}
-
-module.exports = { showProfile, editUser };
+module.exports = { showProfile };
